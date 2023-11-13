744b6ea704c2cda579f431598524ade077e80da1
Matthew Miller <matthew.miller@asmbly.org>
2023-11-13 01:31:31 -0600

    feat: tailwindCSS setup

---
+++ app/postcss.config.js
@@ -0,0 +1,11 @@
+import autoprefixer from 'autoprefixer';
+import tailwind from 'tailwindcss';
+
+const config = {
+    plugins: [
+        tailwind(),
+        autoprefixer(),
+    ]
+};
+
+export default config;
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,4 @@
+<div class="container mx-auto text-center">
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
