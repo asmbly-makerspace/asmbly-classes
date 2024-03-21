2b0926b060452cf374cbc4014700b72410af1fd0
Enoch Riese <enoch.riese@gmail.com>
2024-03-21 11:20:10 -0500

    Use higher contrast primary and accent colors in dark mode

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,3 +1,36 @@
+const colors = {
+	asmbly: {
+		DEFAULT: '#2b2c6b',
+		hover: '#7f80cb'
+	},
+	woodwork: {
+		DEFAULT: '#f3b61b',
+		hover: '#f9da8d'
+	},
+	metalwork: {
+		DEFAULT: '#578bc9',
+		hover: '#abc5e4'
+	},
+	textiles: {
+		DEFAULT: '#79ccc4',
+		hover: '#bce5e1'
+	},
+	electronics: {
+		DEFAULT: '#00916e',
+		hover: '#48ffd3'
+	},
+	lasers: {
+		DEFAULT: '#d33e43',
+		hover: '#e99ea1'
+	},
+	'3dprinting': {
+		DEFAULT: '#ee6e23',
+		hover: '#f6b691'
+	}
+}
+
+const asmblyWhite = '#f2f4ef'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
@@ -7,36 +40,7 @@ export default {
 		'./src/lib/images/AsmblyLogo.svg',
 	],
 	theme: {
-		colors: {
-			asmbly: {
-				DEFAULT: '#2b2c6b',
-				hover: '#7f80cb'
-			},
-			woodwork: {
-				DEFAULT: '#f3b61b',
-				hover: '#f9da8d'
-			},
-			metalwork: {
-				DEFAULT: '#578bc9',
-				hover: '#abc5e4'
-			},
-			textiles: {
-				DEFAULT: '#79ccc4',
-				hover: '#bce5e1'
-			},
-			electronics: {
-				DEFAULT: '#00916e',
-				hover: '#48ffd3'
-			},
-			lasers: {
-				DEFAULT: '#d33e43',
-				hover: '#e99ea1'
-			},
-			'3dprinting': {
-				DEFAULT: '#ee6e23',
-				hover: '#f6b691'
-			}
-		},
+		colors,
 		extend: {
 			fontFamily: {
 				asmbly: ['asmbly', 'sans-serif'],
@@ -49,18 +53,18 @@ export default {
 		themes: [
 			{
 				asmbly: {
-					primary: '#2b2c6b',
-					secondary: '#f2f4ef',
-					accent: '#2b2c6b',
+					primary: colors.asmbly.DEFAULT,
+					secondary: asmblyWhite,
+					accent: colors.asmbly.hover,
 					neutral: '#3d4451',
-					'base-100': '#f2f4ef'
+					'base-100': asmblyWhite
 				}
 			},
 			{
 				asmblyDark: {
-					primary: '#2b2c6b',
-					secondary: '#f2f4ef',
-					accent: '#f2f4ef',
+					primary: asmblyWhite,
+					secondary: colors.asmbly.DEFAULT,
+					accent: colors.asmbly.hover,
 					neutral: '#7c879c',
 					'base-100': '#3d4451'
 				}
